eaf2553bd36702f851ffbd45677df7d97656953b
EC2 Default User <ec2-user@ip-172-31-17-235.ec2.internal>
2022-01-08 02:49:05 +0000

    [Add] 更新機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to list_path(books.id)
   end
 
   def index
+   @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @list = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
